2ae99b909c2090ba78fd8086016dca34352d9e7e
iuna Leon <yuna.ads@hotmail.com>
2023-06-04 20:13:51 -0300

    test

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('oi');
+var nome = 'Iuna';
+var idade = 32;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Leon', 
+cidade = 'Floripa';
+console.log(sobrenome, cidade);
+
+var nome = 'Iuna'
+var idade = '32'
+var comidaFavorita;
+comidaFavorita = 'yakisoba'
+
+//declarar 5 variáveis diferentes sem valores
+var n1,
+    n2,
+    n3,
+    n4,
+    n5;
+
+console.log(nome, idade, comidaFavorita, n1,n2,n3,n4,n5);
